a446fc9b30225547621af46ca63d4fc757c731dd
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-04-02 13:16:15 +0200

    [IMP][BACKEND] Create sensors app

---
+++ backend/sensors/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Dimension(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Tick(models.Model):
+    timestamp = models.DateTimeField()
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    dimension = models.ForeignKey(Dimension, on_delete=models.CASCADE)
+    prediction = models.ForeignKey("Prediction", on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.timestamp} - {self.dimension.name}: {self.value}"
+
+
+class Prediction(models.Model):
+    dimension = models.ForeignKey(Dimension, on_delete=models.CASCADE)
+    start_timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.dimension.name} - {self.start_timestamp}"
