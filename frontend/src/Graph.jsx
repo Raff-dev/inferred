e3c8ce0d33236ca028cdc73fdb2495591d557fe0
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-04-05 20:09:48 +0200

    [IMP][frontend] Sensor tabs

--- frontend/src/Graph.jsx
+++ frontend/src/Graph.jsx
@@ -1,9 +1,9 @@
-import { FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import { FormControlLabel, Radio, RadioGroup, Tab, Tabs } from '@mui/material';
+import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import { curveCardinal } from 'd3-shape';
 import React, { useEffect, useState } from 'react';
 import { Area, AreaChart, CartesianGrid, Legend, Line, LineChart, XAxis, YAxis } from 'recharts';
-
 
 const DAY = 24 * 60 * 60;
 const HOUR = 60 * 60;
@@ -37,6 +37,8 @@ const ChartView = () => {
   const [windowData, setWindowData] = useState([]);
   const [windowSize, setWindowSize] = useState(DATA_WINDOW_SIZE_DEFAULT);
   const [chartType, setChartType] = useState('line');
+  const [selectedSensor, setSelectedSensor] = useState(null);
+  const [sensors, setSensors] = useState([]);
 
   const handleWindowSizeChange = (event, newValue) => {
     setWindowSize(newValue);
@@ -46,10 +48,21 @@ const ChartView = () => {
     setChartType(event.target.value);
   };
 
+  const handleSensorChange = (event) => {
+    setSelectedSensor(event.target.value);
+  };
+
   useEffect(() => {
     const ws = new WebSocket(WEB_SOCKET_URL);
     ws.onmessage = (event) => {
       const newData = JSON.parse(event.data);
+
+      if (selectedSensor === null) {
+        const sensors = Object.keys(newData.sensors);
+        setSensors(sensors);
+        setSelectedSensor(sensors[0]);
+      }
+
       setData((prevData) => [...prevData, newData]);
       setWindowData((prevData) => [...prevData.slice(-windowSize), newData]);
     };
@@ -57,43 +70,64 @@ const ChartView = () => {
     return () => {
       ws.close();
     };
-  }, [windowSize]);
+  }, [windowSize, selectedSensor]);
 
-  const sensorKey = "sensor_0";
-  const dataKey = `sensors.${sensorKey}.value`;
-  const predictionKey = `sensors.${sensorKey}.prediction[99]`;
+  const dataKey = `sensors.${selectedSensor}.value`;
+  const predictionKey = `sensors.${selectedSensor}.prediction[99]`;
   const props = { data: windowData, dataKey: dataKey, predictionKey: predictionKey, chartType: chartType }
 
   return (
     <div >
+      <SensorTabs sensors={sensors} selectedSensor={selectedSensor} setSelectedSensor={setSelectedSensor} />
       <RadioGroup row value={chartType} onChange={handleChartTypeChange} >
         <FormControlLabel value="line" control={<Radio />} label="Line" />
         <FormControlLabel value="area" control={<Radio />} label="Area" />
       </RadioGroup>
       <div style={{ backgroundColor: '#0003', borderRadius: 12, padding: 20, paddingRight: 50, marginBottom: 20 }}>
-        <h2>{sensorKey}</h2>
+        <h2>{selectedSensor}</h2>
         <MyCommonChart {...props} />
+        <Slider
+          value={windowSize}
+          size="small"
+          min={20}
+          max={MINUTE}
+          step={5}
+          marks
+          valueLabelDisplay="auto"
+          onChange={handleWindowSizeChange}
+        />
       </div>
-      <Slider
-        value={windowSize}
-        size="small"
-        min={20}
-        max={MINUTE}
-        step={5}
-        marks
-        valueLabelDisplay="auto"
-        onChange={handleWindowSizeChange}
-      />
     </div>
   );
 };
+
+
+const SensorTabs = ({ sensors, selectedSensor, setSelectedSensor }) => {
+  const handleChange = (event, newValue) => {
+    setSelectedSensor(newValue);
+  };
+
+  return (
+    <Box  >
+      <Tabs
+        value={selectedSensor}
+        onChange={handleChange}
+        aria-label="Sensor Tabs"
+        variant="scrollable">
+        {sensors.map((sensor) => (
+          <Tab key={sensor} label={sensor} value={sensor} />
+        ))}
+      </Tabs>
+    </Box>
+  );
+}
 
 const MyCommonChart = ({ data, dataKey, predictionKey, chartType }) => {
   const [Chart, ChartElement] = CHARTS[chartType];
 
   return (
     <Chart
-      width={1000}
+      width={1200}
       height={400}
       data={data}
       margin={{
