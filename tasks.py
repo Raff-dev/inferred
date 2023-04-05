e1923308d0e36e62d0a516d4dac576255a9d8a75
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-04-05 19:38:26 +0200

    [IMP][ops] Invoke tasks docker shortcuts

--- tasks.py
+++ tasks.py
@@ -10,27 +10,31 @@ CONTAINER_SHORT_NAMES = {
 @task
 def logs(ctx: Context, container_name: str):
     name = CONTAINER_SHORT_NAMES.get(container_name, container_name)
-    ctx.run(f"docker compose logs -f {name}")
+    ctx.run(f"docker compose logs -f {name}", pty=True)
 
 
 @task
 def up(ctx: Context):
-    ctx.run("docker compose up -d")
+    ctx.run("docker compose up -d", pty=True)
 
 
 @task
 def down(ctx: Context):
-    ctx.run("docker compose down --remove-orphans")
+    ctx.run("docker compose down --remove-orphans", pty=True)
 
 
 @task
-def restart(ctx: Context, container_name: str = None):
-    name = CONTAINER_SHORT_NAMES.get(container_name)
-    ctx.run(f"docker compose restart {name or ''}")
+def restart(ctx: Context, container_name: str):
+    ctx.run(f"docker compose restart {container_name or ''}", pty=True)
 
 
 @task
 def bash(ctx: Context, container_name: str):
     name = CONTAINER_SHORT_NAMES.get(container_name)
     print(name)
-    ctx.run(f"docker compose exec -it {name} bash")
+    ctx.run(f"docker exec -it {name} bash", pty=True)
+
+
+@task
+def build(ctx: Context):
+    ctx.run("docker compose up -d --build --remove-orphans", pty=True)
