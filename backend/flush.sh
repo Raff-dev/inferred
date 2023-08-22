080d2a647a617d3dcece5daa3a6ce21ed5664c80
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-08-22 21:44:17 +0200

    [IMP][backend] make flush message green

--- backend/flush.sh
+++ backend/flush.sh
@@ -1,3 +1,4 @@
 python manage.py flush --no-input > /dev/null 2>&1
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" > /dev/null 2>&1
-echo "Successfully flushed and created Django superuser 'admin' with password 'admin'"
+message="Successfully flushed and created Django superuser 'admin' with password 'admin'"
+echo -e "\e[32m$message\e[0m"
