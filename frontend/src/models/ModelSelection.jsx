d554cad2363d3bab4279f1e42141df78d676e4cf
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-08-22 23:37:00 +0200

    [IMP][frontend] require selecting at least one model

--- frontend/src/models/ModelSelection.jsx
+++ frontend/src/models/ModelSelection.jsx
@@ -1,14 +1,24 @@
 import { Chip, MenuItem, Select } from "@mui/material";
 import Box from "@mui/material/Box";
 import React from "react";
+
 const ModelSelection = ({ modelNames, selectedModels, onModelSelect }) => {
+    const handleModelChange = (event) => {
+        const newSelectedModels = event.target.value;
+
+        // Check if unselecting would result in no models selected
+        if (selectedModels.length > 1 || newSelectedModels.length > 0) {
+            onModelSelect(event);
+        }
+    };
+
     return (
         <Box sx={{ minWidth: 120 }} marginTop={3}>
             <Select
                 sx={{ width: "400px" }}
                 multiple
                 value={selectedModels}
-                onChange={onModelSelect}
+                onChange={handleModelChange} // Use the custom handler
                 renderValue={(selected) => (
                     <div>
                         {selected.map((model) => (
