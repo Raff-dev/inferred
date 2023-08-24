e004b1ee906ed57083d1bb64e5a495307fa9ab4b
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-08-24 19:39:48 +0200

    [IMP][backend] register simulation model in admin

--- backend/inferred/sensors/admin.py
+++ backend/inferred/sensors/admin.py
@@ -1,8 +1,9 @@
 # Register your models here.
 from django.contrib import admin
 
-from inferred.sensors.models import Dimension, Prediction, Tick
+from inferred.sensors.models import Dimension, Prediction, SimulationModel, Tick
 
+admin.site.register(SimulationModel)
 admin.site.register(Dimension)
 admin.site.register(Prediction)
 admin.site.register(Tick)
