6c9c9a6593902c70f50b64ae98d54d8adb7e5bca
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-08-26 13:12:43 +0200

    [FIX][backend] Properly create admin user while flushing

--- backend/flush.sh
+++ backend/flush.sh
@@ -1,8 +1,9 @@
 python manage.py flush --no-input > /dev/null 2>&1
+python manage.py makemigrations
+python manage.py migrate
+
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" > /dev/null 2>&1
 message="Successfully flushed and created Django superuser 'admin' with password 'admin'"
 echo -e "\e[32m$message\e[0m"
 
-python manage.py makemigrations
-python manage.py migrate
 python manage.py loaddata inferred/sensors/fixtures/initial_data.json
