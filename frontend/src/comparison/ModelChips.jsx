8b95df061b612f89473f2c975fd60fa2ad50c754
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-09-11 21:52:59 +0200

    [REF][frontend] Rename ModelSelect to ModelChips

---
+++ frontend/src/comparison/ModelChips.jsx
@@ -0,0 +1,39 @@
+import { Chip, MenuItem, Select } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import React from "react";
+
+const ModelChips = ({ modelNames, selectedModels, onModelSelect }) => {
+    const handleModelChange = (event) => {
+        const newSelectedModels = event.target.value;
+
+        // Check if unselecting would result in no models selected
+        if (selectedModels.length > 1 || newSelectedModels.length > 0) {
+            onModelSelect(event);
+        }
+    };
+
+    return (
+        <FormControl sx={{ minWidth: 400 }}>
+            <Select
+                multiple
+                value={selectedModels}
+                onChange={handleModelChange}
+                renderValue={(selected) => (
+                    <div>
+                        {selected.map((model) => (
+                            <Chip key={model} label={model} />
+                        ))}
+                    </div>
+                )}
+            >
+                {modelNames.map((model) => (
+                    <MenuItem key={model} value={model}>
+                        {model}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    );
+};
+
+export default ModelChips;
