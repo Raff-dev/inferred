90ed7721ff784270517da5ceaa3b9f30ab68ac83
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-10-09 09:48:09 +0200

    [IMP][realdata] move API key to .env

--- realdata/weather.py
+++ realdata/weather.py
@@ -1,8 +1,9 @@
+import os
 import time
 
 import requests
 
-API_KEY = "b2fc395fdccaf979b0d9fa1da0fcee1c"
+API_KEY = os.environ.get("OPEN_WEATHER_MAP_API_KEY")
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 CITY = "Warsaw,pl"
 INTERVAL = 1
